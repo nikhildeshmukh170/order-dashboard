94093a48c574b8216558b4367614bf63a05d5303
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2025-01-14 18:36:11 +0530

    update

--- admin-dashboard/app/admin/orders/page.tsx
+++ admin-dashboard/app/admin/orders/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
   return (
     <section className="w-full rounded-2xl bg-white p-7">
       <div className="flex flex-wrap items-center justify-between gap-2">
-        <h2 className="text-xl font-semibold">All Books</h2>
+        <h2 className="text-xl font-semibold">All Orders</h2>
         <Button className="bg-primary-admin" asChild>
           <Link href="/admin/orders/new" className="text-white">
             + Create a New Order
